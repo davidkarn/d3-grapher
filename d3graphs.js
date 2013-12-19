9689bf32946e0369c406508de07fce2d75798837
David Karn <=>
2013-12-19 13:49:04 -0800

    Adding graphing functions

--- d3graphs.js
+++ d3graphs.js
@@ -1,4 +1,11 @@
-
+// DATA MANIPULATION FUNCTIONS
+//
+// Graphs will begin with data in either an array, or a hash of subhashes, such as:
+//
+// [{time: '01:00', width: 3.4}, {time: 02:00, width: 4.4}]
+// or
+// {'01:00': {time: '01:00', width: 3.4}, 
+//  '02:00': {time: 02:00, width: 4.4}}
 
 // split an array into an array of subarrays of <interval> length
 // split_every([1,2,3,4,5,6,7,8,9], 3) => 
@@ -20,7 +27,38 @@ function hashify_array(array) {
 	    o[arguments[i]] = sub_array[i - 1]; }
 	return o; }); }
 
+function array_to_keyed_hash(array, key) {
+    var o = {};
+    array.each(function(sub_array) {
+	o[sub_array[key]] = sub_array; }); 
+    return o; }
 
+function hashes_merge_data() {
+    var hash = arguments[0]; 
+    for (var i = 1; i < arguments.length; i++) {
+	var hash2 = arguments[i];
+	for (var j in hash2) {
+	    if (!hash[j]) {
+		hash[j] = {}; }
+	    for (var key in hash2[j]) {
+		hash[j][key] = hash2[j][key]; }}}
+    return hash; }
+	    
+
+// GRAPHING FUNCTIONS
+//
+// Calling create_graph(svg, data, options) will create a graph object with functions
+// for drawing charts and modules for addons. 
+//
+// Functions drawing to the svg should draw in layers by applying classes to all DOM elements 
+// and select()ing only elements of their layer (like axes with labels, charts, legends, etc).
+// This way multiple charts can be overlapped or combined without moving around the wrong elements.
+	    
+
+function create_graph(svg, data) {
+    var graph = {};
+
+    return graph; }
 
 if (Meteor.isClient) {
   Template.hello.greeting = function () {
